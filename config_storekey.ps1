8753fbfc005238911423c555b2a187854917ab33
berndinox <berndinox@gmail.com>
2023-04-11 21:11:26 +0200

    Errorhandling

--- config_storekey.ps1
+++ config_storekey.ps1
@@ -2,6 +2,6 @@ param (
     [Parameter(Mandatory=$true)]
     [string]$KEY
 )
-if ($variable -eq $null) {Write-Error "Store Key not set"; break}
+if ($KEY -eq $null) {Write-Error "Store Key not set"; break}
 try { & .\psexec.exe -s -accepteula 'C:\Program Files\FSLogix\Apps\frx.exe' add-secure-key -key='connectionString' -value=$KEY }
 catch {Write-Error "Executing frx.exe via psexec failed"}
