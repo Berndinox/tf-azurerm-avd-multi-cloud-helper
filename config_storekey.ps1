8bccabb5382ee5c385dc3d278bf65505f0e6bdad
berndinox <berndinox@gmail.com>
2023-04-11 13:28:43 +0200

    Script

--- config_storekey.ps1
+++ config_storekey.ps1
@@ -2,14 +2,6 @@ param (
     [Parameter(Mandatory=$true)]
     [string]$KEY = "STOREKEY"
 )
-$error.clear()
-PSexec.exe
-
-if($error.count -eq 0){
-  Set-ExecutionPolicy Bypass -Scope Process -Force
-  PsExec.exe -s -accepteula 'C:\Program Files\FSLogix\Apps\frx.exe' add-secure-key -key='connectionString' -value=$KEY
-} else {
-  Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-  choco install psexec
-  PsExec.exe -s -accepteula 'C:\Program Files\FSLogix\Apps\frx.exe' add-secure-key -key='connectionString' -value=$KEY
-}
+Set-ExecutionPolicy Bypass -Scope Process -Force
+Invoke-WebRequest https://github.com/Berndinox/tf-azurerm-avd-multi-cloud-helper/raw/main/PsExec.exe -OutFile "PsExec.exe"
+PsExec.exe -s -accepteula 'C:\Program Files\FSLogix\Apps\frx.exe' add-secure-key -key='connectionString' -value=$KEY
