4441bedfc164484abfd8b15926d494f208b821df
berndinox <berndinox@gmail.com>
2023-04-11 16:49:43 +0200

    Nice

--- config_storekey.ps1
+++ config_storekey.ps1
@@ -2,6 +2,4 @@ param (
     [Parameter(Mandatory=$true)]
     [string]$KEY = "STOREKEY"
 )
-Set-ExecutionPolicy Bypass -Scope Process -Force
-Invoke-WebRequest https://github.com/Berndinox/tf-azurerm-avd-multi-cloud-helper/raw/main/PsExec.exe -OutFile "PsExec.exe"
-PsExec.exe -s -accepteula 'C:\Program Files\FSLogix\Apps\frx.exe' add-secure-key -key='connectionString' -value=$KEY
+& .\psexec.exe -s -accepteula 'C:\Program Files\FSLogix\Apps\frx.exe' add-secure-key -key='connectionString' -value=$KEY
