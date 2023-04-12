8dfc56f6db5c0c35c617416b972f31283a7f1433
berndinox <berndinox@gmail.com>
2023-04-12 13:19:30 +0200

    Refactore

--- config_storekey.ps1
+++ config_storekey.ps1
@@ -2,18 +2,22 @@ param (
     [Parameter(Mandatory=$true)]
     [string]$KEY
 )
+
+Write-Output "FSLogix Config Start"
+
 if ($KEY -eq $null) {Write-Output "Store Key not set"}
 else {
     try {
-        Write-Host "Executing frx.exe via psexec"
-        & .\psexec.exe -s -accepteula 'C:\Program Files\FSLogix\Apps\frx.exe' add-secure-key -key=connectionString -value=$KEY
-    } catch { Write-Output "Executing frx.exe via psexec failed" }
+        Write-Output "Executing frx.exe"
+        Start-Process -FilePath "C:\Program Files\FSLogix\Apps\frx.exe" -Wait -ArgumentList "add-secure-key","-key=connectionString","-value=","$Key"
+        Write-Output "Key Set: $Key"
+    } catch { Write-Output "Executing frx.exe failed" }
 }
 
-Write-Output 'Configure FSLogix ...'
+Write-Output 'Set RegHives'
 New-ItemProperty -Path HKLM:\Software\FSLogix\Profiles -Name 'Enabled' -PropertyType:DWORD -Value 1 -Force
 New-ItemProperty -Path "HKLM:\Software\FSLogix\Profiles" -Name "CCDLocations" -PropertyType:String -Value "type=azure,connectionString=""|fslogix/connectionString|""" -Force
 New-ItemProperty -Path HKLM:\Software\FSLogix\Profiles -Name 'VolumeType' -PropertyType:String -Value vhdx -Force
 New-ItemProperty -Path HKLM:\Software\FSLogix\Profiles -Name 'DeleteLocalProfileWhenVHDShouldApply' -PropertyType:dword -Value 1 -Force
 New-ItemProperty -Path HKLM:\Software\FSLogix\Profiles -Name 'FlipFlopProfileDirectoryName' -PropertyType:dword -Value 1 -Force
-Write-Output 'FsLogix complete ...'
+Write-Output "FSLogix Config Finished"
