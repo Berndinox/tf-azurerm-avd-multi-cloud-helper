0ab141e842a894fb53f307f36567baba5d73f143
berndinox <berndinox@gmail.com>
2023-04-12 11:50:00 +0200

    Yes

--- config_storekey.ps1
+++ config_storekey.ps1
@@ -3,5 +3,5 @@ param (
     [string]$KEY
 )
 if ($KEY -eq $null) {Write-Error "Store Key not set"; break}
-try { & .\psexec.exe -s -accepteula 'C:\Program Files\FSLogix\Apps\frx.exe' add-secure-key -key='connectionString' -value=$KEY }
+try {& .\psexec.exe -s -accepteula 'C:\Program Files\FSLogix\Apps\frx.exe' add-secure-key -key=connectionString -value=$KEY}
 catch {Write-Error "Executing frx.exe via psexec failed"}
